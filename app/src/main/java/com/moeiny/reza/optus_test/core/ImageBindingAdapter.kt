803138f76379f3c5d90b28ffb404013d282c9100
reza575 <h_moeiny@yahoo.com>
2021-04-21 22:59:43 +1000

    Complete project

---
+++ app/src/main/java/com/moeiny/reza/optus_test/core/ImageBindingAdapter.kt
@@ -0,0 +1,18 @@
+package com.moeiny.reza.optus_test.core
+
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+import com.squareup.picasso.Picasso
+
+/*
+ * this object is essential to showing image on ImageView for viewitems of ViewAdapter
+    when we use databinding
+*/
+
+object ImageBindingAdapter {
+    @JvmStatic
+    @BindingAdapter("imageUrl")
+    fun setImageUrl(view: ImageView, url: String) {
+        Picasso.get().load(url).into(view)
+    }
+}
---
+++ app/src/main/java/com/moeiny/reza/optus_test/data/model/uimodel/ShowPhotoModel.kt
@@ -0,0 +1,9 @@
+package com.moeiny.reza.optus_test.data.model.uimodel
+
+data class ShowPhotoModel(
+    val albumId: String?,
+    val id: String?,
+    val thumbnailUrl: String?,
+    val title: String?,
+    val url: String?
+)
